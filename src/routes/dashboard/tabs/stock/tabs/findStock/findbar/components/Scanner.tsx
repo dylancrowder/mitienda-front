59ced6665a56329a82ba30d1f7fcf44b614bb1a6
dylan <devdylancrowder@outlook.com>
2025-02-04 21:05:23 -0300

    fps

--- src/routes/dashboard/tabs/stock/tabs/findStock/findbar/components/Scanner.tsx
+++ src/routes/dashboard/tabs/stock/tabs/findStock/findbar/components/Scanner.tsx
@@ -17,9 +17,9 @@ const Scanner: React.FC<BarcodeScannerProps> = ({ onDetected }) => {
 
             const scanner = new Html5QrcodeScanner(
                 scannerId,
-                { 
-                    fps: 60,  // Incrementar el FPS para mejor rendimiento
-                    qrbox: { width: 450, height: 450 },
+                {
+                    fps: 10,  // Reducir FPS a 30 para mejorar rendimiento
+                    qrbox: { width: 300, height: 300 },  // Reducir el tamaño del área de escaneo
                 },
                 false
             );
@@ -30,8 +30,7 @@ const Scanner: React.FC<BarcodeScannerProps> = ({ onDetected }) => {
             };
 
             const handleError = (error: string) => {
-             console.log(error);
-             
+                console.log("Error en la detección:", error);
             };
 
             // Iniciar escáner
@@ -44,7 +43,8 @@ const Scanner: React.FC<BarcodeScannerProps> = ({ onDetected }) => {
         }
     }, [onDetected]);
 
-    return <div ref={scannerRef}></div>;
+    return <div ref={scannerRef} >
+    </div>;
 };
 
 export default Scanner;
